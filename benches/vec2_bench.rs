d7e1601d8168a0a700e2d8ff9202f71a5cde937e
Andrea Greco <113620544+mrMart1an@users.noreply.github.com>
2023-05-13 17:06:30 +0200

    inital commit

---
+++ benches/vec2_bench.rs
@@ -0,0 +1,38 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use vecrs::vectors::Vec2;
+
+
+// Becnhmrk the normalization function
+pub fn vec2_normilze(c: &mut Criterion) {
+    let a = black_box(Vec2::new(1.,2.));
+
+    c.bench_function("vec2 normalize", |b| b.iter(|| a.normalize()));
+}
+
+// Benchmark the dot product function
+pub fn vec2_dot(c: &mut Criterion) {
+    let a_vec = black_box(Vec2::new(1.,2.));
+    let b_vec = black_box(Vec2::new(2.,3.));
+
+    c.bench_function("vec2 dot", |b| b.iter(|| Vec2::dot(&a_vec, &b_vec)));
+}
+
+// Benchmark the add function
+pub fn vec2_add(c: &mut Criterion) {
+    let a_vec = black_box(Vec2::new(1.,2.));
+    let b_vec = black_box(Vec2::new(2.,3.));
+
+    c.bench_function("vec2 add", |b| b.iter(|| &a_vec + &b_vec));
+}
+
+// Create the benchmarks group
+criterion_group!(
+    benches, 
+    
+    vec2_normilze,
+    vec2_dot,
+    vec2_add,
+);
+
+// Run the benchmarks
+criterion_main!(benches);
