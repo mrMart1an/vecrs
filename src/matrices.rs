217296d749c53a130137eeb069d32ef0b2a4e83f
Andrea Greco <andreagreco2004@outlook.com>
2023-05-21 16:24:44 +0200

    matrix dot

--- src/matrices.rs
+++ src/matrices.rs
@@ -1,21 +1,21 @@
 use std::ops;
 
 // Define the 2 by 2 matrix struct
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct Mat2x2 {
-    matrix: [[f64; 2]; 2],
+    m: [[f64; 2]; 2],
 }
 
 // Define the 3 by 3 matrix struct
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct Mat3x3 {
-    matrix: [[f64; 3]; 3],
+    m: [[f64; 3]; 3],
 }
 
 // Define the 4 by 4 matrix struct
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct Mat4x4 {
-    matrix: [[f64; 4]; 4],
+    m: [[f64; 4]; 4],
 }
 
 // Implement constructor and method form Mat2x2
@@ -23,7 +23,7 @@ impl Mat2x2 {
     /// Create a new matrix initialized with 
     /// the elements in the float array
     pub fn new(matrix: [[f64; 2]; 2]) -> Self {
-        Self { matrix }
+        Self { m: matrix }
     }
 
     /// Create a 3 by 3 identity matrix
@@ -33,6 +33,29 @@ impl Mat2x2 {
             [0., 1.],
         ])
     }
+
+    /// Perform the matrix multiplication of two matrices
+    /// and return the reuslt as a new matrix
+    pub fn dot(a: &Mat2x2, b: &Mat2x2) -> Self {
+        Self::new([[
+            a[0][0]*b[0][0] + a[0][1]*b[1][0],
+            a[0][0]*b[0][1] + a[0][1]*b[1][1],
+        ],[
+            a[1][0]*b[0][0] + a[1][1]*b[1][0],
+            a[1][0]*b[0][1] + a[1][1]*b[1][1],
+        ]])
+    }
+
+
+    /// Performe the matrix multiplication of two matrices
+    /// and save the result in a mutable referecete to Mat2x2
+    pub fn dot_ref(a: &Mat2x2, b: &Mat2x2, out: &mut Mat2x2) {
+        out[0][0] = a[0][0]*b[0][0] + a[0][1]*b[1][0];
+        out[0][1] = a[0][0]*b[0][1] + a[0][1]*b[1][1];
+
+        out[1][0] = a[1][0]*b[0][0] + a[1][1]*b[1][0];
+        out[1][1] = a[1][0]*b[0][1] + a[1][1]*b[1][1];
+    }
 }
 
 // Implement constructor and method form Mat3x3
@@ -40,7 +63,7 @@ impl Mat3x3 {
     /// Create a new matrix initialized with 
     /// the elements in the float array
     pub fn new(matrix: [[f64; 3]; 3]) -> Self {
-        Self { matrix }
+        Self { m: matrix }
     }
 
     /// Create a 3 by 3 identity matrix
@@ -51,6 +74,41 @@ impl Mat3x3 {
             [0., 0., 1.],
         ])
     }
+    
+    /// Perform the matrix multiplication of two matrices
+    /// and return the reuslt as a new matrix
+    pub fn dot(a: &Mat3x3, b: &Mat3x3) -> Self {
+        Self::new([[
+            a[0][0]*b[0][0] + a[0][1]*b[1][0] + a[0][2]*b[2][0],
+            a[0][0]*b[0][1] + a[0][1]*b[1][1] + a[0][2]*b[2][1],
+            a[0][0]*b[0][2] + a[0][1]*b[1][2] + a[0][2]*b[2][2],
+        ],[
+            a[1][0]*b[0][0] + a[1][1]*b[1][0] + a[1][2]*b[2][0],
+            a[1][0]*b[0][1] + a[1][1]*b[1][1] + a[1][2]*b[2][1],
+            a[1][0]*b[0][2] + a[1][1]*b[1][2] + a[1][2]*b[2][2],
+        ],[
+            a[2][0]*b[0][0] + a[2][1]*b[1][0] + a[2][2]*b[2][0],
+            a[2][0]*b[0][1] + a[2][1]*b[1][1] + a[2][2]*b[2][1],
+            a[2][0]*b[0][2] + a[2][1]*b[1][2] + a[2][2]*b[2][2],
+        ]])
+    }
+
+
+    /// Performe the matrix multiplication of two matrices
+    /// and save the result in a mutable referecete to Mat3x3
+    pub fn dot_ref(a: &Mat3x3, b: &Mat3x3, out: &mut Mat3x3) {
+        out[0][0] = a[0][0]*b[0][0] + a[0][1]*b[1][0] + a[0][2]*b[2][0];
+        out[0][1] = a[0][0]*b[0][1] + a[0][1]*b[1][1] + a[0][2]*b[2][1];
+        out[0][2] = a[0][0]*b[0][2] + a[0][1]*b[1][2] + a[0][2]*b[2][2];
+
+        out[1][0] = a[1][0]*b[0][0] + a[1][1]*b[1][0] + a[1][2]*b[2][0];
+        out[1][1] = a[1][0]*b[0][1] + a[1][1]*b[1][1] + a[1][2]*b[2][1];
+        out[1][2] = a[1][0]*b[0][2] + a[1][1]*b[1][2] + a[1][2]*b[2][2];
+
+        out[2][0] = a[2][0]*b[0][0] + a[2][1]*b[1][0] + a[2][2]*b[2][0];
+        out[2][1] = a[2][0]*b[0][1] + a[2][1]*b[1][1] + a[2][2]*b[2][1];
+        out[2][2] = a[2][0]*b[0][2] + a[2][1]*b[1][2] + a[2][2]*b[2][2];
+    }
 }
 
 // Implement constructor and method form Mat4x4
@@ -58,7 +116,7 @@ impl Mat4x4 {
     /// Create a new matrix initialized with 
     /// the elements in the float array
     pub fn new(matrix: [[f64; 4]; 4]) -> Self {
-        Self { matrix }
+        Self { m: matrix }
     }
 
     /// Create a 3 by 3 identity matrix
@@ -70,6 +128,60 @@ impl Mat4x4 {
             [0., 0., 0., 1.],
         ])
     }
+
+    /// Perform the matrix multiplication of two matrices
+    /// and return the reuslt as a new matrix
+    pub fn dot(a: &Mat4x4, b: &Mat4x4) -> Self {
+        Self::new([[
+            a[0][0]*b[0][0] + a[0][1]*b[1][0] + a[0][2]*b[2][0] + a[0][3]*b[3][0],
+            a[0][0]*b[0][1] + a[0][1]*b[1][1] + a[0][2]*b[2][1] + a[0][3]*b[3][1],
+            a[0][0]*b[0][2] + a[0][1]*b[1][2] + a[0][2]*b[2][2] + a[0][3]*b[3][2],
+            a[0][0]*b[0][3] + a[0][1]*b[1][3] + a[0][2]*b[2][3] + a[0][3]*b[3][3],
+        ],[
+            a[1][0]*b[0][0] + a[1][1]*b[1][0] + a[1][2]*b[2][0] + a[1][3]*b[3][0],
+            a[1][0]*b[0][1] + a[1][1]*b[1][1] + a[1][2]*b[2][1] + a[1][3]*b[3][1],
+            a[1][0]*b[0][2] + a[1][1]*b[1][2] + a[1][2]*b[2][2] + a[1][3]*b[3][2],
+            a[1][0]*b[0][3] + a[1][1]*b[1][3] + a[1][2]*b[2][3] + a[1][3]*b[3][3],
+        ],[
+            a[2][0]*b[0][0] + a[2][1]*b[1][0] + a[2][2]*b[2][0] + a[2][3]*b[3][0],
+            a[2][0]*b[0][1] + a[2][1]*b[1][1] + a[2][2]*b[2][1] + a[2][3]*b[3][1],
+            a[2][0]*b[0][2] + a[2][1]*b[1][2] + a[2][2]*b[2][2] + a[2][3]*b[3][2],
+            a[2][0]*b[0][3] + a[2][1]*b[1][3] + a[2][2]*b[2][3] + a[2][3]*b[3][3],
+        ],[
+            a[3][0]*b[0][0] + a[3][1]*b[1][0] + a[3][2]*b[2][0] + a[3][3]*b[3][0],
+            a[3][0]*b[0][1] + a[3][1]*b[1][1] + a[3][2]*b[2][1] + a[3][3]*b[3][1],
+            a[3][0]*b[0][2] + a[3][1]*b[1][2] + a[3][2]*b[2][2] + a[3][3]*b[3][2],
+            a[3][0]*b[0][3] + a[3][1]*b[1][3] + a[3][2]*b[2][3] + a[3][3]*b[3][3],
+        ]])
+    }
+
+
+    /// Performe the matrix multiplication of two matrices
+    /// and save the result in a mutable referecete to Mat4x4
+    pub fn dot_ref(a: &Mat4x4, b: &Mat4x4, out: &mut Mat4x4) {
+        // Abomination but faster than a for loop
+        out[0][0] = a[0][0]*b[0][0] + a[0][1]*b[1][0] + a[0][2]*b[2][0] + a[0][3]*b[3][0];
+        out[0][1] = a[0][0]*b[0][1] + a[0][1]*b[1][1] + a[0][2]*b[2][1] + a[0][3]*b[3][1];
+        out[0][2] = a[0][0]*b[0][2] + a[0][1]*b[1][2] + a[0][2]*b[2][2] + a[0][3]*b[3][2];
+        out[0][3] = a[0][0]*b[0][3] + a[0][1]*b[1][3] + a[0][2]*b[2][3] + a[0][3]*b[3][3];
+
+        out[1][0] = a[1][0]*b[0][0] + a[1][1]*b[1][0] + a[1][2]*b[2][0] + a[1][3]*b[3][0];
+        out[1][1] = a[1][0]*b[0][1] + a[1][1]*b[1][1] + a[1][2]*b[2][1] + a[1][3]*b[3][1];
+        out[1][2] = a[1][0]*b[0][2] + a[1][1]*b[1][2] + a[1][2]*b[2][2] + a[1][3]*b[3][2];
+        out[1][3] = a[1][0]*b[0][3] + a[1][1]*b[1][3] + a[1][2]*b[2][3] + a[1][3]*b[3][3];
+
+        out[2][0] = a[2][0]*b[0][0] + a[2][1]*b[1][0] + a[2][2]*b[2][0] + a[2][3]*b[3][0];
+        out[2][1] = a[2][0]*b[0][1] + a[2][1]*b[1][1] + a[2][2]*b[2][1] + a[2][3]*b[3][1];
+        out[2][2] = a[2][0]*b[0][2] + a[2][1]*b[1][2] + a[2][2]*b[2][2] + a[2][3]*b[3][2];
+        out[2][3] = a[2][0]*b[0][3] + a[2][1]*b[1][3] + a[2][2]*b[2][3] + a[2][3]*b[3][3];
+
+        out[3][0] = a[3][0]*b[0][0] + a[3][1]*b[1][0] + a[3][2]*b[2][0] + a[3][3]*b[3][0];
+        out[3][1] = a[3][0]*b[0][1] + a[3][1]*b[1][1] + a[3][2]*b[2][1] + a[3][3]*b[3][1];
+        out[3][2] = a[3][0]*b[0][2] + a[3][1]*b[1][2] + a[3][2]*b[2][2] + a[3][3]*b[3][2];
+        out[3][3] = a[3][0]*b[0][3] + a[3][1]*b[1][3] + a[3][2]*b[2][3] + a[3][3]*b[3][3];
+
+    }
+
 }
 
 // Index operator implementations
@@ -79,7 +191,13 @@ impl ops::Index<usize> for Mat2x2 {
     type Output = [f64; 2];
     
     fn index(&self, index: usize) -> &Self::Output {
-        &self.matrix[index]
+        &self.m[index]
+    }
+}
+// Implement the mutable index operator for Mat2x2
+impl ops::IndexMut<usize> for Mat2x2 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.m[index]
     }
 }
 
@@ -88,7 +206,13 @@ impl ops::Index<usize> for Mat3x3 {
     type Output = [f64; 3];
     
     fn index(&self, index: usize) -> &Self::Output {
-        &self.matrix[index]
+        &self.m[index]
+    }
+}
+// Implement the mutable index operator for Mat3x3
+impl ops::IndexMut<usize> for Mat3x3 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.m[index]
     }
 }
 
@@ -97,6 +221,12 @@ impl ops::Index<usize> for Mat4x4 {
     type Output = [f64; 4];
     
     fn index(&self, index: usize) -> &Self::Output {
-        &self.matrix[index]
+        &self.m[index]
+    }
+}
+// Implement the mutable index operator for Mat4x4
+impl ops::IndexMut<usize> for Mat4x4 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.m[index]
     }
 }
--- tests/mat_tests.rs
+++ tests/mat_tests.rs
@@ -1,1 +1,88 @@
-// TODO add matrices tests
+use vecrs::matrices::{Mat2x2, Mat3x3, Mat4x4};
+
+#[test]
+fn mat2x2_dot() {
+    let a = Mat2x2::new([
+        [1., 2.],
+        [3., 4.],
+    ]);
+    let b = Mat2x2::new([
+        [2., 3.],
+        [4., 5.],
+    ]);
+    
+    // The correct result
+    let result = Mat2x2::new([
+        [10., 13.],
+        [22., 29.],
+    ]);
+    
+    // check the dot fucntion
+    assert_eq!(Mat2x2::dot(&a, &b), result);
+
+    // ouput matrix for the dot_ref operation
+    let mut output = Mat2x2::default();
+    Mat2x2::dot_ref(&a, &b, &mut output);
+    assert_eq!(output, result);
+}
+
+#[test]
+fn mate3x3_dot() {
+    let a = Mat3x3::new([
+        [1., 2., 3.],
+        [4., 5., 6.],
+        [7., 8., 9.],
+    ]);
+    let b = Mat3x3::new([
+        [2., 3., 4.],
+        [5., 6., 7.],
+        [8., 9., 10.],
+    ]);
+    
+    // The correct result
+    let result = Mat3x3::new([
+        [36., 42., 48.],
+        [81., 96., 111.],
+        [126., 150., 174.],
+    ]);
+    
+    // check the dot fucntion
+    assert_eq!(Mat3x3::dot(&a, &b), result);
+
+    // ouput matrix for the dot_ref operation
+    let mut output = Mat3x3::default();
+    Mat3x3::dot_ref(&a, &b, &mut output);
+    assert_eq!(output, result);
+}
+
+#[test]
+fn mate4x4_dot() {
+    let a = Mat4x4::new([
+        [1., 2., 3., 4.],
+        [5., 6., 7., 8.],
+        [9., 10., 11., 12.],
+        [13., 14., 15., 16.],
+    ]);
+    let b = Mat4x4::new([
+        [2., 3., 4., 5.],
+        [6., 7., 8., 9.],
+        [10., 11., 12., 13.],
+        [14., 15., 16., 17.],
+    ]);
+    
+    // The correct result
+    let result = Mat4x4::new([
+        [100., 110., 120., 130.],
+        [228., 254., 280., 306.],
+        [356., 398., 440., 482.],
+        [484., 542., 600., 658.],
+    ]);
+    
+    // check the dot fucntion
+    assert_eq!(Mat4x4::dot(&a, &b), result);
+
+    // ouput matrix for the dot_ref operation
+    let mut output = Mat4x4::default();
+    Mat4x4::dot_ref(&a, &b, &mut output);
+    assert_eq!(output, result);
+}
